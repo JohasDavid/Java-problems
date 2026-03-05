9a80ca8291437a62be4181914b58c46a345d0a4e
JohasDavid <johas@Johass-Mac-mini.local>
2026-03-05 17:09:08 -0600

    Polymorphism problem 2

--- src/PolymorphismExercises/PolymorphismExercises.java
+++ src/PolymorphismExercises/PolymorphismExercises.java
@@ -16,10 +16,16 @@ public class PolymorphismExercises {
         for(Animal animal : list){
             animal.makeSound();
         }
-
-
         // 2. Crea una clase Shape con el método calculateArea(). Luego implementa subclases Circle y Rectangle con sus propias fórmulas. Usa una lista de Shape para recorrer e imprimir el área de varias figuras.
+        Circle circle = new Circle(100);
+        Rectangle rectangle = new Rectangle(10, 20);
+        Rectangle rectangle1 = new Rectangle(20,10);
 
+        List<Shape> shapes = new ArrayList<Shape>(List.of(circle, rectangle, rectangle1));
+
+        for(Shape s : shapes){
+            s.calculateArea();
+        }
         // 3. Crea una clase Printer con varios métodos print() sobrecargados que acepten diferentes tipos de parámetros (String, int, double). Llama a cada uno desde main.
 
         // 4. Crea una clase Greeter con dos métodos greet(): uno que salude con “Hello”, y otro que reciba un nombre y salude con “Hello, [nombre]”.
@@ -35,6 +41,37 @@ public class PolymorphismExercises {
         // 9. Crea una clase Product con el método getPrice(). Luego, Book y Electronic deben sobrescribirlo con su propia lógica de descuento. Recorre una lista de Product e imprime el precio final de cada uno.
 
         // 10. Crea una clase Character con método attack(). Luego crea subclases Warrior, Archer, Mage con ataques diferentes. En main, crea un array de Character y llama a attack() para cada uno.
+    }
+
+    public static class Shape{
+        public void calculateArea(){
+            System.out.println("Calculating area: ");
+        }
+    }
+
+    public static class Circle extends Shape{
+        private double radius;
+        Circle(double radius){
+            this.radius = radius;
+        }
+
+        @Override
+        public void calculateArea(){
+            System.out.println("Area: " + (this.radius * this.radius * Math.PI));
+        }
+    }
+
+    public static class Rectangle extends Shape{
+        private double width, height;
+        Rectangle(double width, double height){
+            this.width = width;
+            this.height = height;
+        }
+
+        @Override
+        public void calculateArea(){
+            System.out.println("Area: " + (this.height * this.width));
+        }
     }
 
     public static class Animal{
