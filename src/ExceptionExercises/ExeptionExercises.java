72dc790a540dd237afa0d38ea200f9558362903c
JohasDavid <johas@Johass-Mac-mini.local>
2026-03-17 20:18:43 -0600

    ExceptionExercises creation and problem 1

---
+++ src/ExceptionExercises/ExeptionExercises.java
@@ -0,0 +1,43 @@
+package ExceptionExercises;
+
+public class ExeptionExercises {
+
+    public static void main(String[] args) {
+
+        // 1. Divide dos números almacenados en dos variables. Maneja la división por cero con try-catch.
+        int a = 5;
+        int b = 20;
+        int c = 0;
+        division(b, a);
+        division(a, b);
+        division(c, a);
+        division(a, c);
+
+        // 2. Crea un array de 3 elementos e intenta acceder al índice 5. Captura el ArrayIndexOutOfBoundsException.
+
+        // 3. Crea una variable String nula e intenta imprimir su longitud. Maneja el NullPointerException.
+
+        // 4. Escribe una función que transforma texto a número. Usa try-catch para manejar entradas no válidas (NumberFormatException).
+
+        // 5. Escribe un programa con un bloque finally que se ejecute siempre, haya o no error.
+
+        // 6. Usa throw para lanzar un IllegalArgumentException si un número introducido es negativo.
+
+        // 7. Crea una clase TemperatureChecker que lanza una excepción personalizada si la temperatura es menor a -50 o mayor a 50.
+
+        // 8. Crea un programa con varios bloques catch: uno para ArithmeticException, otro para ArrayIndexOutOfBoundsException.
+
+        // 9. Crea una función checkPassword(String pass) que lance una excepción si la contraseña es demasiado corta.
+
+        // 10. Implementa una clase LoginSystem que use una excepción personalizada LoginFailedException si el usuario o contraseña son incorrectos.
+    }
+
+    public static void division(int a, int b){
+        try{
+            int c = a / b;
+            System.out.printf("%d / %d = %d\n", a, b, c);
+        } catch (ArithmeticException e) {
+            System.out.println("No se puede dividir por 0.");
+        }
+    }
+}
